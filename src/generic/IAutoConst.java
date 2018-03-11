f78b38c56076c28f94fee835b4fe52be95fcc159
Suhas M Y <suhasmy29@gmail.com>
2018-03-11 17:49:33 +0530

    Initial commit

---
+++ src/generic/IAutoConst.java
@@ -0,0 +1,14 @@
+package generic;
+
+public interface IAutoConst {
+	String CHROME_KEY="webdriver.chrome.driver";
+	String CHROME_VALUE="./driver/chromedriver.exe";
+
+	String GECKO_KEY="webdriver.gecko.driver";
+	String GECKO_VALUE="./driver/geckodriver.exe";
+	String XL_PATH="./data/Book1.xlsx";
+	String CONFIG_PATH ="./Property/config.properties";
+	String IMG_PATH = "./ScreenShots/";
+
+
+}
